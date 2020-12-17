66e5280180f7f433fb4398aa23f6f04a85abb565
Alexander Smirnov <ru.alexander.smirnov@gmail.com>
2020-12-17 19:53:41 +0300

    optimized 706B

--- 1100/706B.cpp
+++ 1100/706B.cpp
@@ -1,34 +1,11 @@
+#include <bits/stdc++.h>
+
 #include <algorithm>
 #include <iostream>
 #include <string>
 #include <unordered_map>
 #include <vector>
 using namespace std;
-
-int num_less_or_equal(vector<int> vec, int n, int k) {
-
-    int index;
-    int min_index = 0;
-    int max_index = n - 1;
-
-    int num_less = -1;
-
-    while (min_index <= max_index) {
-        index = (max_index + min_index) / 2;
-
-        if (k >= vec[index]) {
-            num_less = index;
-            min_index = index + 1;
-
-        } else if (k < vec[index]) {
-            max_index = index - 1;
-        }
-    }
-
-    /* memory[k] = num_less + 1; */
-
-    return num_less + 1;
-}
 
 vector<int> get_markets_num(int prices_num, vector<int> prices, int days_num,
                             vector<int> money) {
@@ -37,8 +14,10 @@ vector<int> get_markets_num(int prices_num, vector<int> prices, int days_num,
     sort(prices.begin(), prices.end());
 
     for (auto amount : money) {
-        markets_num.push_back(num_less_or_equal(prices, prices_num, amount));
+        markets_num.push_back(
+            upper_bound(prices.begin(), prices.end(), amount) - prices.begin());
     }
+
     return markets_num;
 }
 
