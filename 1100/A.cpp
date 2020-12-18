d0dc095f30027eab157a9430e81af5718dd83c22
Alexander Smirnov <ru.alexander.smirnov@gmail.com>
2020-12-18 19:27:54 +0300

    bitwise operations testing

---
+++ 1100/A.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define pb push_back
+#define fori(i, n) for (int i = 0; i < n; ++i)
+
+typedef long long ll;
+typedef vector<long long> vi;
+typedef pair<long long, long long> pi;
+
+double eps = 1e-9;
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    ll n;
+    cin >> n;
+
+    cout << (7<<1);
+
+    /* fori(b, (1 << n)) { */
+    /*     vi subset; */
+    /*     cout << "b=" << b << "\n"; */
+    /*     fori(i, n) { */
+    /*         if (b & (1 << i)) subset.pb(i); */
+    /*     } */
+    /*     cout << "subset:" << "\n"; */
+    /*     for (auto k : subset) cout << k << " "; */
+    /*     cout << "\n"; */
+    /*     cout << "\n"; */
+    /* } */
+
+    /* vi vec; */
+    /* ll elem; */
+    /* fori(i, n) { */
+    /*     cin >> elem; */
+    /*     vec.pb(elem); */
+    /* } */
+}
